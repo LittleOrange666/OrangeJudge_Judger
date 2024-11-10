f033726863663a0ade4b26b217d04861e0b0f35b
LittleOrange666 <orangeminecraft123@gmail.com>
2024-11-10 23:16:25 +0800

    save changes

--- autoinit.sh
+++ autoinit.sh
@@ -2,3 +2,5 @@ sudo apt-get update
 sudo apt-get -y install python3
 sudo apt-get -y install python3-pip
 sudo pip3 install -r requirements.txt
+sudo useradd judge -u 1500 --system --no-create-home
+sudo useradd compile -u 1600 --system --no-create-home
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-import fastapi
+from fastapi import FastAPI, Body, File, UploadFile
 import uvicorn
 import asyncio
 from pydantic import BaseModel
 
-app = fastapi.FastAPI()
+from modules import executing, task
 
-
-class TestRequest(BaseModel):
-    pass
+app = FastAPI()
 
 
 @app.post("/test")
-async def test(request: TestRequest):
+async def test():
     await asyncio.sleep(100)
     return {"result": "ok"}
 
+
+@app.post("/run")
+async def run(lang: str = Body(...), file: UploadFile = File(...), in_file: UploadFile = File(...)):
+    with open(f"tmp/{file.filename}", "wb") as f:
+        f.write(file.file.read())
+    file.file.close()
+    with open(f"tmp/{in_file.filename}", "wb") as f:
+        f.write(in_file.file.read())
+    in_file.file.close()
+    res = task.run(f"tmp/{file.filename}", lang, f"tmp/{in_file.filename}")
+    return res
+
 if __name__ == '__main__':
+    executing.init()
     uvicorn.run(app, host='0.0.0.0', port=8000)
