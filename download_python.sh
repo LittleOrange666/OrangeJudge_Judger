2acab89dbd9da003d5219b762eddfdc244a1811a
LittleOrange666 <orangeminecraft123@gmail.com>
2024-11-11 21:02:31 +0800

    a usable version

--- download_python.sh
+++
@@ -1,4 +0,0 @@
-wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
-wget https://www.python.org/ftp/python/3.10.14/Python-3.10.14.tgz
-wget https://downloads.python.org/pypy/pypy3.6-v7.3.2-linux64.tar.bz2
-wget https://downloads.python.org/pypy/pypy3.10-v7.3.16-linux64.tar.bz2
--- run_test.sh
+++
@@ -1,2 +0,0 @@
-BASEDIR=$(dirname $0)
-docker run -p 9132:8000 -v "${BASEDIR}/sandbox":/sandbox --rm judger_server_test
