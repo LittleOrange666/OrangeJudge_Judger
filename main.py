d3067b1ba693eeb67dca3d1042664a22d2116806
LittleOrange666 <orangeminecraft123@gmail.com>
2024-11-15 20:36:30 +0800

    add cwd and cmds in request args

--- main.py
+++ main.py
@@ -24,13 +24,17 @@ class CallRequest(BaseModel):
     user: User | None = None
     stdin: str = ""
     timeout: float | None = None
+    cwd: str | None = None
+    cmds: list[list[str]] | None = None
 
 
 @app.post("/call")
 async def call(item: CallRequest, token: str = Header(...)):
     print(item.model_dump())
     check_token(token)
-    res = executing.call(**item.model_dump())
+    if item.cmds is not None:
+        executing.calls(item.cmds, item.cwd)
+    res = executing.call(**item.model_dump(exclude={"cmds"}))
     return res
 
 
@@ -43,13 +47,17 @@ class JudgeRequest(BaseModel):
     err_file: str = "/dev/null"
     seccomp_rule_name: SeccompRule | None = None
     uid: int = constants.nobody_uid
+    cwd: str | None = None
+    cmds: list[list[str]] | None = None
 
 
 @app.post("/judge")
 async def judge(item: JudgeRequest, token: str = Header(...)) -> Result:
     print(item.model_dump())
     check_token(token)
-    res = executing.run(**item.model_dump())
+    if item.cmds is not None:
+        executing.calls(item.cmds, item.cwd)
+    res = executing.run(**item.model_dump(exclude={"cmds"}))
     return res
 
 
@@ -66,13 +74,17 @@ class InteractJudgeRequest(BaseModel):
     interact_seccomp_rule_name: SeccompRule | None = None
     uid: int = constants.nobody_uid
     interact_uid: int = constants.nobody_uid
+    cwd: str | None = None
+    cmds: list[list[str]] | None = None
 
 
 @app.post("/interact_judge")
 async def interact_judge(item: InteractJudgeRequest, token: str = Header(...)) -> InteractResult:
     print(item.model_dump())
     check_token(token)
-    res = executing.interact_run(**item.model_dump())
+    if item.cmds is not None:
+        executing.calls(item.cmds, item.cwd)
+    res = executing.interact_run(**item.model_dump(exclude={"cmds"}))
     return res
 
 
--- modules/executing.py
+++ modules/executing.py
@@ -41,9 +41,14 @@ class InteractResult:
     interact_result: Result
 
 
+def calls(cmds: list[list[str]], cwd: str | None = None):
+    for cmd in cmds:
+        subprocess.call(cmd, cwd=cwd)
+
+
 def run(cmd: list[str], tl: int = 1000, ml: int = 128, in_file: str = "/dev/null", out_file: str = "/dev/null",
         err_file: str = "/dev/null", seccomp_rule_name: str | None = None,
-        uid: int = constants.nobody_uid, reverse_io: int = 0) -> Result:
+        uid: int = constants.nobody_uid, reverse_io: int = 0, cwd: str | None = None) -> Result:
     # tl: ms ml: MB
     exe_path = cmd[0]
     if not exe_path.startswith("/"):
@@ -51,6 +56,9 @@ def run(cmd: list[str], tl: int = 1000, ml: int = 128, in_file: str = "/dev/null
         if which_exe_path is not None:
             exe_path = which_exe_path
     log_file = temp_filename()
+    old_cwd = os.getcwd()
+    if cwd is not None:
+        os.chdir(cwd)
     ret = _judger.run(max_cpu_time=tl,
                       max_real_time=tl + 1000,
                       max_memory=ml * 1024 * 1024,
@@ -72,6 +80,7 @@ def run(cmd: list[str], tl: int = 1000, ml: int = 128, in_file: str = "/dev/null
                       uid=uid,
                       gid=uid,
                       reverse_io=reverse_io)
+    os.chdir(old_cwd)
     ret["result_id"] = ret["result"]
     ret["error_id"] = ret["error"]
     ret["result"] = constants.judegr_result.get(ret["result"], "unknown")
@@ -88,11 +97,14 @@ def interact_run(cmd: list[str], interact_cmd: list[str], tl: int = 1000, ml: in
                  out_file: str = "/dev/null",
                  err_file: str = "/dev/null", interact_err_file: str = "/dev/null",
                  seccomp_rule_name: str | None = None, interact_seccomp_rule_name: str | None = None,
-                 uid: int = constants.nobody_uid, interact_uid: int = constants.nobody_uid):
+                 uid: int = constants.nobody_uid, interact_uid: int = constants.nobody_uid, cwd: str | None = None):
     fifo1 = temp_filename()
     fifo2 = temp_filename()
     os.mkfifo(fifo1)
     os.mkfifo(fifo2)
+    old_cwd = os.getcwd()
+    if cwd is not None:
+        os.chdir(cwd)
     inter_res: Result | None = None
 
     def run_inter():
@@ -104,17 +116,19 @@ def interact_run(cmd: list[str], interact_cmd: list[str], tl: int = 1000, ml: in
     t.start()
     res = run(cmd, tl, ml, fifo2, fifo1, err_file, seccomp_rule_name, uid)
     t.join()
+    os.chdir(old_cwd)
     return InteractResult(result=res, interact_result=inter_res)
 
 
-def call(cmd: list[str], user: User | None = None, stdin: str = "", timeout: float | None = None) -> tuple[
+def call(cmd: list[str], user: User | None = None, stdin: str = "", timeout: float | None = None,
+         cwd: str | None = None) -> tuple[
     str, str, int]:
     if user is not None:
         cmd = ["sudo", "-u", user.value] + cmd
     print(*cmd)
     if timeout is None:
         timeout = 30
-    process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
     try:
         ret = process.communicate(stdin.encode("utf8"), timeout=timeout)
         return ret[0].decode("utf8"), ret[1].decode("utf8"), process.returncode
